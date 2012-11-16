ee5364db046b4157ce3239991f8e24ef11462da3
rrae <rrae@localhorst.(none)>
2012-11-16 21:10:22 +0100

    Proper AJAX support in RType

--- question/type/rtypetask/module.js
+++ question/type/rtypetask/module.js
@@ -1,24 +1,24 @@
 // Note you should disable Javascript Caching during Development!
 // http://docs.moodle.org/dev/Developer_Mode
 
-// http://stackoverflow.com/questions/2419749/get-selected-elements-outer-html
-(function($) {
-	$.fn.outerHTML = function() {
-		return $(this).clone().wrap('<div></div>').parent().html();
-	}
-})(jQuery);
-
-var click_on_addanswer = function() {
-	var num_subquestion = this.name.substring("addanswerbtn_".length);
+var click_handler = function() {
+	// find out the number of the current question -> to find more than one (number), the regex would end with /g
+	var num_subquestion = this.name.match(/[0-9]+/);
+	// submit subsequent form (AJAX)
 	var rqparams = $("form").serialize();
 	rqparams += "&json_request_for_subquestion="+num_subquestion;
-	rqparams += "&"+this.name+"="+this.value; // addanswerbtn_X was not serialize()d
-	// submit subsequent form (AJAX)
+	rqparams += "&"+this.name+"="+this.value; // the button's name was not serialize()d
 	$.post("type/rtypetask/ajaxiface.php", rqparams, function(data, textStatus, jqXHR) {
 		try { // replace section with returned HTML
 			var $section = $(data).find("section");
 			$('section[id="answers_for_question_'+num_subquestion+'"]').replaceWith($section);
-			$('input[name^="addanswerbtn_"]').click(click_on_addanswer);
+			// re-Bind action listeners, note that ^ is a "begins with"-selector
+			$('input[name="addanswerbtn_'+num_subquestion+'"]').click(click_handler);
+			$('input[name^="delanswerbtn_'+num_subquestion+'_"]').click(click_handler);
+			// change the num_answers field
+			var selector = 'input[name="num_answers_'+num_subquestion+'"]',
+				value = $(data).find(selector).val();
+			$(selector).val(value);
 		} catch (e) {
 			console.log(e);
 		}
@@ -27,5 +27,6 @@ var click_on_addanswer = function() {
 }
 
 $(document).ready(function() {
-	$('input[name^="addanswerbtn_"]').click(click_on_addanswer);
+	$('input[name^="addanswerbtn_"]').click(click_handler);
+	$('input[name^="delanswerbtn_"]').click(click_handler);
 });
