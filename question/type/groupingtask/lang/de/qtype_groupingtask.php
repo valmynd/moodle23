83d1d281ae865797741d84663fb65ab04685a297
rrae <rrae@localhorst.(none)>
2012-11-26 00:12:20 +0100

    Qtype CLOZE/multianswer actually has a dependancy on Numerical

---
+++ question/type/groupingtask/lang/de/qtype_groupingtask.php
@@ -0,0 +1,30 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'qtype_groupingtask', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package    qtype
+ * @subpackage groupingtask
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'GroupingTask';
+$string['pluginname_help'] = 'In diesem Aufgabentyp geht es darum, eine Reihe von Stichworten mit Hilfe von Verbindungslinien in einen Zusammenhang zu bringen.';
+$string['pluginname_link'] = 'question/type/groupingtask';
+$string['pluginnameadding'] = 'Eine GroupingTask Aufgabe hinzufügen';
+$string['pluginnameediting'] = 'Eine GroupingTask Aufgabe editieren';
+$string['pluginnamesummary'] = 'In diesem Aufgabentyp geht es darum, eine Reihe von Stichworten mit Hilfe von Verbindungslinien in einen Zusammenhang zu bringen.';
---
+++ question/type/timetask/lang/de/qtype_timetask.php
@@ -0,0 +1,29 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'qtype_timetask', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package    qtype
+ * @subpackage timetask
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+$string['pluginname'] = 'TimeTask';
+$string['pluginname_help'] = 'Bei diesem Aufgabentyp sind eine Reihe von Ereignissen auf einem Zeitstrahl anzuordnen..';
+$string['pluginname_link'] = 'question/type/timetask';
+$string['pluginnameadding'] = 'Eine TimeTask Aufgabe hinzufügen';
+$string['pluginnameediting'] = 'Eine TimeTask Aufgabe editieren';
+$string['pluginnamesummary'] = 'Bei diesem Aufgabentyp sind eine Reihe von Ereignissen auf einem Zeitstrahl anzuordnen.';
